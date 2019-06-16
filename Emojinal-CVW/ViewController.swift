57accfa4d641b0797a0448f1dfae2d4d83223f6e
Cheyenne Vlymen-Williams <cheyennevwilliams@gmail.com>
2019-06-16 10:42:07 -0400

    Complete Iteration 3

--- Emojinal-CVW/ViewController.swift
+++ Emojinal-CVW/ViewController.swift
@@ -21,7 +21,14 @@ class ViewController: UIViewController {
         // pseudo-code the things that need to happen
         // when a user clicks an emoji button
 
-        let alertController = UIAlertController(title: "Got it!", message: "\(emojis[selectedEmotion!]!)", preferredStyle: .alert)
+        let number = Int.random(in: 0...2)
+        
+        let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[number]
+            // here I want to extract the word that goes with my emoji
+            // then find that word in the dictionary
+            // get it's value (array)
+            // and access the first element
+        let alertController = UIAlertController(title: "Got it!", message: "\(emojiMessage!)", preferredStyle: .alert)
         
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         
@@ -30,5 +37,8 @@ class ViewController: UIViewController {
     }
     
     let emojis = ["😝": "You Feel Silly", "🙃": "You Feel Fake Fine"]
+    
+    var customMessages = ["You Feel Silly": ["Do a little dance", "Spead a little love", "Get down tonight"], "You Feel Fake Fine":["You're almost through the day", "Get something to eat", "Remind yourself why you're here"]]
+
 }
 
